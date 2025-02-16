bae7e5b8e16c8fefc8941b17012eeacdf8e2c083
PedroTrudes <pedro.jtrudes@gmail.com>
2025-02-16 15:28:19 -0300

    :sparkles: feat: Creation of banner component and floating logo done / opening animation completed.

--- src/js/animationLoad.js
+++ src/js/animationLoad.js
@@ -1,4 +1,4 @@
-const text = document.querySelectorAll('.textAnimationOne');
+const textOne = document.querySelectorAll('.textAnimationOne');
 const textTwo = document.querySelectorAll('.textAnimationTwo');
 const textThree = document.querySelectorAll('.textAnimationThree');
 const textFour = document.querySelectorAll('.textAnimationFour')
@@ -6,12 +6,15 @@ const containerAnimationOne = document.querySelector('.containerLoadOne');
 const containerAnimationTwo = document.querySelector('.containerLoadTwo');
 const containerAnimationThree = document.querySelector('.containerLoadThree');
 const containerAnimationFour = document.querySelector('.containerLoadFour');
+const containerWebHeader = document.querySelector(".header");
+const containerWebPage = document.querySelector(".main");
+const containerLoad = document.querySelector(".load");
 
 setTimeout(() => {
     containerAnimationOne.style.display = "none";
     containerAnimationTwo.style.display = "flex";
     
-}, 4000)
+}, 4500)
 
 setTimeout(() => {
     containerAnimationTwo.style.display = "none";
@@ -23,17 +26,23 @@ setTimeout(() => {
     containerAnimationFour.style.display = "flex";
 }, 13000)
 
+setTimeout(() => {
+    containerLoad.style.display = "none"; 
+    containerWebHeader.style.display = "block";
+    containerWebPage.style.display = "block";
+}, 18000)
 
-text.forEach((text, i) => {
+
+textOne.forEach((text, i) => {
     text.style.animationDelay = `${i * 0.1}s`;
 })
 
 textTwo.forEach((text, i) => {
-    text.style.animationDelay = `${i * 0.1}s`;
+    text.style.animationDelay = `${i * 0.08}s`;
 })
 
 textThree.forEach((text, i) => {
-    text.style.animationDelay = `${i * 0.1}s`;
+    text.style.animationDelay = `${i * 0.08}s`;
 })
 
 textFour.forEach((text, i) => {
