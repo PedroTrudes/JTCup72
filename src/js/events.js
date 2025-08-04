024a327981185b96c0f47745ae2738950fcc77c7
PedroTrudes <pedro.jtrudes@gmail.com>
2025-08-03 22:57:32 -0300

    :wastebasket: remove: displayNone in components / :card_file_box: raw: events.js with data example waiting manipulation

---
+++ src/js/events.js
@@ -0,0 +1,13 @@
+const events = [
+  { id: 1, nameEvent: "Truck Day", dtEvent: "2025-08-20", timeEvent: "10:00AM", nameMonth: "Agosto" },
+  { id: 2, nameEvent: "Feira de Livros", dtEvent: "2025-09-05", timeEvent: "09:00AM", nameMonth: "Setembro" },
+  { id: 3, nameEvent: "Hackathon Universitário", dtEvent: "2025-09-22", timeEvent: "08:00AM", nameMonth: "Setembro" },
+  { id: 4, nameEvent: "Festival de Música", dtEvent: "2025-10-10", timeEvent: "18:00PM", nameMonth: "Outubro" },
+  { id: 5, nameEvent: "Oficina de Robótica", dtEvent: "2025-10-25", timeEvent: "13:30PM", nameMonth: "Outubro" },
+  { id: 6, nameEvent: "Congresso de Tecnologia", dtEvent: "2025-11-04", timeEvent: "10:00AM", nameMonth: "Novembro" },
+  { id: 7, nameEvent: "Encontro de Desenvolvedores", dtEvent: "2025-11-18", timeEvent: "14:00PM", nameMonth: "Novembro" },
+  { id: 8, nameEvent: "Feira de Games", dtEvent: "2025-12-01", timeEvent: "12:00PM", nameMonth: "Dezembro" },
+  { id: 9, nameEvent: "Apresentação de Natal", dtEvent: "2025-12-15", timeEvent: "19:30PM", nameMonth: "Dezembro" },
+  { id: 10, nameEvent: "Corrida de Rua", dtEvent: "2025-08-31", timeEvent: "07:00AM", nameMonth: "Agosto" },
+  { id: 11, nameEvent: "Exposição de Arte", dtEvent: "2025-10-05", timeEvent: "11:00AM", nameMonth: "Outubro" }
+];
