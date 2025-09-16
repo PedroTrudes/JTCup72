36e086f687ea44d67ea737216401f7e26f2ea894
PedroTrudes <pedro.jtrudes@gmail.com>
2025-09-16 11:57:54 -0300

    :broom: cleanup: event information removed

--- src/js/events.js
+++ src/js/events.js
@@ -1,15 +1,7 @@
 const events = [
-  { id: 1, nameEvent: "Truck Day", dtEvent: "2025-08-20", timeEvent: "10:00AM", nameMonth: "Agosto", localizationEvent: "registro-SP" },
-  { id: 2, nameEvent: "Corrida de Rua", dtEvent: "2025-08-31", timeEvent: "07:00AM", nameMonth: "Agosto", localizationEvent: "registro-SP" },
-  { id: 3, nameEvent: "Feira de Livros", dtEvent: "2025-09-05", timeEvent: "09:00AM", nameMonth: "Setembro", localizationEvent: "registro-SP" },
-  { id: 4, nameEvent: "Hackathon Universitário", dtEvent: "2025-09-22", timeEvent: "08:00AM", nameMonth: "Setembro", localizationEvent: "registro-SP" },
-  { id: 5, nameEvent: "Exposição de Arte", dtEvent: "2025-10-05", timeEvent: "11:00AM", nameMonth: "Outubro", localizationEvent: "registro-SP" },
-  { id: 6, nameEvent: "Festival de Música", dtEvent: "2025-10-10", timeEvent: "18:00PM", nameMonth: "Outubro", localizationEvent: "registro-SP" },
-  { id: 7, nameEvent: "Oficina de Robótica", dtEvent: "2025-10-25", timeEvent: "13:30PM", nameMonth: "Outubro", localizationEvent: "registro-SP" },
-  { id: 8, nameEvent: "Congresso de Tecnologia", dtEvent: "2025-11-04", timeEvent: "10:00AM", nameMonth: "Novembro", localizationEvent: "registro-SP" },
-  { id: 9, nameEvent: "Encontro de Desenvolvedores", dtEvent: "2025-11-18", timeEvent: "14:00PM", nameMonth: "Novembro", localizationEvent: "registro-SP" },
-  { id: 10, nameEvent: "Feira de Games", dtEvent: "2025-12-01", timeEvent: "12:00PM", nameMonth: "Dezembro", localizationEvent: "registro-SP" },
-  { id: 11, nameEvent: "Apresentação de Natal", dtEvent: "2025-12-15", timeEvent: "19:30PM", nameMonth: "Dezembro", localizationEvent: "registro-SP" },
+  { id: 1, nameEvent: "Treino livre", dtEvent: "2025-10-05", timeEvent: "11:00AM", nameMonth: "Outubro", localizationEvent: "registro-SP" },
+  { id: 2, nameEvent: "Pista livre", dtEvent: "2025-10-10", timeEvent: "18:00PM", nameMonth: "Outubro", localizationEvent: "registro-SP" },
+  { id: 3, nameEvent: "Track Day", dtEvent: "2025-10-25", timeEvent: "13:30PM", nameMonth: "Outubro", localizationEvent: "registro-SP" },
 ];
 
 
