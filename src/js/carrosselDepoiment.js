8880bfc816f8216c89db717c9e9c0777c0b320a4
PedroTrudes <pedro.jtrudes@gmail.com>
2025-10-07 10:51:06 -0300

    :goal_net: Adjustment in carrossel/Depoiment

--- src/js/carrosselDepoiment.js
+++ src/js/carrosselDepoiment.js
@@ -1,39 +1,50 @@
-const carrossel = document.querySelector(".carrosselDepoiment");
-const btnPrev = document.querySelector(".btnPrev");
-const btnNext = document.querySelector(".btnNext");
-const card = document.querySelectorAll(".cardDepoiment");
-const containerIndicator = document.querySelector(".carrosselIndicator");
+window.addEventListener("load", () => {
+  const carrossel = document.querySelector(".carrosselDepoiment");
+  const btnPrev = document.querySelector(".btnPrev");
+  const btnNext = document.querySelector(".btnNext");
+  const card = document.querySelectorAll(".cardDepoiment");
+  const containerIndicator = document.querySelector(".carrosselIndicator");
+  const main = document.querySelector("main")
 
+  let cardWidth;
+  const observer = new MutationObserver(() => {
+    if (getComputedStyle(main).display !== "none") {
+      cardWidth = card[0].offsetWidth;
+      updateCtas();
+      observer.disconnect(); // para de observar depois de ajustar
+    }
+  });
 
-let cardWidth = card[0].offsetWidth;
+  observer.observe(main, { attributes: true, attributeFilter: ["style"] });
+  console.log("Chamando card" , cardWidth)
 
-function updateCtas(){
+  function updateCtas() {
     const maxScrollLeft = carrossel.scrollWidth - carrossel.clientWidth;
 
-    if(carrossel.scrollLeft <= 20){
-        btnPrev.classList.add("disableButtonCarrossel");
-    }else{
-        btnPrev.classList.remove("disableButtonCarrossel");
+    if (carrossel.scrollLeft <= 20) {
+      btnPrev.classList.add("disableButtonCarrossel");
+    } else {
+      btnPrev.classList.remove("disableButtonCarrossel");
     }
 
-    if(carrossel.scrollLeft >= maxScrollLeft){
-        btnNext.classList.add("disableButtonCarrossel");
-    }else{
-        btnNext.classList.remove("disableButtonCarrossel");
-    } 
-}
+    if (carrossel.scrollLeft >= maxScrollLeft) {
+      btnNext.classList.add("disableButtonCarrossel");
+    } else {
+      btnNext.classList.remove("disableButtonCarrossel");
+    }
+  }
 
-carrossel.addEventListener("scroll", updateCtas );
+  carrossel.addEventListener("scroll", updateCtas);
 
-
-btnNext.addEventListener("click", () => {
-    carrossel.scrollBy({left: cardWidth, behavior: "smooth"});
+  btnNext.addEventListener("click", () => {
+    carrossel.scrollBy({ left: cardWidth, behavior: "smooth" });
     updateCtas();
-})
+  });
 
-btnPrev.addEventListener("click", () => {
-    carrossel.scrollBy({left: -cardWidth, behavior: "smooth"})
+  btnPrev.addEventListener("click", () => {
+    carrossel.scrollBy({ left: -cardWidth, behavior: "smooth" });
     updateCtas();
-})
+  });
 
-updateCtas();
+  updateCtas();
+});
