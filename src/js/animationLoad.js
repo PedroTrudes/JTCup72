102ff4ccc8c993ca3113ae3c6a5b46760541b43b
PedroTrudes <pedro.jtrudes@gmail.com>
2025-10-06 22:36:31 -0300

    vercel testing

--- src/js/animationLoad.js
+++ src/js/animationLoad.js
@@ -3,6 +3,7 @@ const textTwo = document.querySelectorAll('.textAnimationTwo');
 const textThree = document.querySelectorAll('.textAnimationThree');
 const textFour = document.querySelectorAll('.textAnimationFour');
 
+const containerAnimationBackground = document.querySelector('.loadBackground');
 const containerAnimationOne = document.querySelector('.containerLoadOne');
 const containerAnimationTwo = document.querySelector('.containerLoadTwo');
 const containerAnimationThree = document.querySelector('.containerLoadThree');
@@ -30,36 +31,65 @@ textFour.forEach((text, i) => {
   text.style.animationDelay = `${i * 0.1}s`;
 });
 
-// --- CONTROLE DOS CONTAINERS (agora com animationend)
-containerAnimationOne.addEventListener("animationend", (e) => {
-  if (e.animationName === "scaleBackgoundOne") {
+containerAnimationBackground.addEventListener("animationstart", () => {
+  // container 1 começa visível
+  //containerAnimationOne.style.display = "flex";
+
+  setTimeout(() => {
+    containerAnimationOne.style.display = "none";
+    containerAnimationTwo.style.display = "flex";
+  }, 2500); // 25% do tempo da animação (10s * 0.25)
+
+  setTimeout(() => {
+    containerAnimationTwo.style.display = "none";
+    containerAnimationThree.style.display = "flex";
+  }, 5000); // 50%
+
+  setTimeout(() => {
+    containerAnimationThree.style.display = "none";
+    containerAnimationFour.style.display = "flex";
+  }, 7500); // 75%
+
+  setTimeout(() => {
+    containerLoad.style.display = "none"; 
+    containerWebHeader.style.display = "flex";
+    containerWebPage.style.display = "block";
+    containerFooterPage.style.display = "block";
+  }, 10000); // 100%
+});
+
+/*
+
+containerAnimationBackground.addEventListener("animationend", (e) => {
+  if (e.animationName === "scaleBackgroundOne") {
     containerAnimationOne.style.display = "none";
     containerAnimationTwo.style.display = "flex";
   }
 });
 
-containerAnimationTwo.addEventListener("animationend", (e) => {
-  if (e.animationName === "scaleBackgoundTwo") {
+containerAnimationBackground.addEventListener("animationend", (e) => {
+  if (e.animationName === "scaleBackgroundTwo") {
     containerAnimationTwo.style.display = "none";
     containerAnimationThree.style.display = "flex";
   }
 });
 
-containerAnimationThree.addEventListener("animationend", (e) => {
-  if (e.animationName === "scaleBackgoundThree") {
+containerAnimationBackground.addEventListener("animationend", (e) => {
+  if (e.animationName === "scaleBackground") {
     containerAnimationThree.style.display = "none";
     containerAnimationFour.style.display = "flex";
   }
 });
 
-containerAnimationFour.addEventListener("animationend", (e) => {
-  if (e.animationName === "scaleBackgoundFour") {
+containerAnimationBackground.addEventListener("animationend", (e) => {
+  if (e.animationName === "scaleBackground") {
     containerLoad.style.display = "none"; 
     containerWebHeader.style.display = "flex";
     containerWebPage.style.display = "block";
     containerFooterPage.style.display = "block";
   }
 });
+*/
 
 /* 
 const textOne = document.querySelectorAll('.textAnimationOne');
